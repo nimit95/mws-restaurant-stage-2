3c5a737c108c750c3cd8f5eeb32590dd7ba11ce1
Nimit <nimitagg95@gmail.com>
2018-11-18 20:41:50 +0530

    bugs fixed
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- js/registerSW.js
+++ js/registerSW.js
@@ -1,7 +1,5 @@
 
-navigator.serviceWorker.register('sw.js', {
-  scope: './'
-}).then(reg => {
+navigator.serviceWorker.register('sw.js').then(reg => {
   console.log('Service Worker Registration Successful: ' + reg.scope);
 })
 .catch(error => {
